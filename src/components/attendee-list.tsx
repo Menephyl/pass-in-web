f54b3ac66a7ec8b7028d8f8dc426f329a4d0c5fa
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-10 20:13:47 -0300

    Update attendee-list.tsx

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -29,66 +29,6 @@ const [search, setSearch] = useState(() => {
   const totalPages = Math.ceil(attendees.length)/10
 
 
-  // const url = new URL(window.location.toString());
-
-  // if (url.searchParams.has("search")) {
-  //   return url.searchParams.get("search") ?? "";
-  // }
-
-  // return "";
-});
-// const [page, setPage] = useState(() => {
-//   const url = new URL(window.location.toString());
-
-//   if (url.searchParams.has("page")) {
-//     return Number(url.searchParams.get("page"));
-//   }
-
-//   return 1;
-// });
-
-// const [total, setTotal] = useState(0);
-// const [attendees, setAttendees] = useState Attendee[]>([]);
-
-// const totalPages = Math.ceil(total / 10);
-
-// useEffect(() => {
-//   const url = new URL(
-//     "http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees"
-//   );
-
-//   url.searchParams.set("pageIndex", String(page - 1));
-//   if (search.length > 1) {
-//     url.searchParams.set("query", search);
-//   }
-
-//   fetch(url)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       setAttendees(data.attendees);
-//       setTotal(data.total);
-//     });
-// }, [page, search]);
-
-// function setCurrentSearch(search: string) {
-//   const url = new URL(window.location.toString());
-
-//   url.searchParams.set("search", search);
-
-//   window.history.pushState({}, "", url);
-
-//   setSearch(search);
-// }
-
-// function setCurrentPage(page: number) {
-//   const url = new URL(window.location.toString());
-
-//   url.searchParams.set("page", String(page));
-
-//   window.history.pushState({}, "", url);
-
-//   setPage(page);
-// }
 
 function onSearchInputChanged(event:ChangeEvent<HTMLInputElement>) {
   setSearch(event.target.value);
