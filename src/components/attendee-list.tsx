b8eb516394baa26b7e2ac8c99bc5ea0b235146bf
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-10 20:12:06 -0300

    Update attendee-list.tsx

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -28,10 +28,105 @@ const [search, setSearch] = useState(() => {
   const totalPages = Math.ceil(attendees.length)/10
 
 
- 
+  // const url = new URL(window.location.toString());
+
+  // if (url.searchParams.has("search")) {
+  //   return url.searchParams.get("search") ?? "";
+  // }
+
+  // return "";
+});
+// const [page, setPage] = useState(() => {
+//   const url = new URL(window.location.toString());
+
+//   if (url.searchParams.has("page")) {
+//     return Number(url.searchParams.get("page"));
+//   }
+
+//   return 1;
+// });
+
+// const [total, setTotal] = useState(0);
+// const [attendees, setAttendees] = useState Attendee[]>([]);
+
+// const totalPages = Math.ceil(total / 10);
+
+// useEffect(() => {
+//   const url = new URL(
+//     "http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees"
+//   );
+
+//   url.searchParams.set("pageIndex", String(page - 1));
+//   if (search.length > 1) {
+//     url.searchParams.set("query", search);
+//   }
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       setAttendees(data.attendees);
+//       setTotal(data.total);
+//     });
+// }, [page, search]);
+
+// function setCurrentSearch(search: string) {
+//   const url = new URL(window.location.toString());
+
+//   url.searchParams.set("search", search);
+
+//   window.history.pushState({}, "", url);
+
+//   setSearch(search);
+// }
+
+// function setCurrentPage(page: number) {
+//   const url = new URL(window.location.toString());
+
+//   url.searchParams.set("page", String(page));
+
+//   window.history.pushState({}, "", url);
+
+//   setPage(page);
+// }
+
+function onSearchInputChanged(event:ChangeEvent<HTMLInputElement>) {
+  setSearch(event.target.value);
+
+}
+function goToFirstPage() {
+  setPage(1);
+}
+
+function goToLastPage() {
+  setPage(totalPages);
+}
+
+function goToPreviousPage() {
+  setPage(page - 1);
+}
+
+function goToNextPage() {
+  setPage(page + 1);
+}
 
 
+    return (
+        <div className='flex flex-col gap-4'>
+            <div className="flex gap-3 items-center">
+              <h1 className="text-2xl font-bold">Participantes</h1> 
+               <div className="px-3  w-72 py-1.5 border border-white/10  rounded-lg text-sm flex items-center gap-3">
+                 <Search className="size-4 text-emerald-300"/>
+                 <input
+            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm"
+            placeholder="Buscar participante..."
+            onChange={onSearchInputChanged}
+          />
+        </div>
+        {search}
+      </div>
 
+
+             <Table>
                
                <thead>
              
@@ -50,5 +145,75 @@ const [search, setSearch] = useState(() => {
                   <TableHeader style={{width:64}} ></TableHeader>
                   </tr>
                   </thead>
-          
-          
+               <tbody>
+                {attendees.map((ateendee) => {
+                 return(
+                  <TableRow key={attendee.id} >
+
+                <TableCell >
+                  <input type="checkbox" className='size-4 bg-black/20 rounded border border-white/10' />
+                </TableCell>
+                  <TableCell >{attendee.id}</TableCell>
+                <TableCell > <div className='flex flex-col gap-1'>
+               <span className="font-semibold text-white">
+                  {attendee.name}
+               </span>
+              <span>{attendee.email}</span>
+            </div>
+                </TableCell>
+           
+                    <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
+                    <TableCell>
+                  {ateendee.checkedInAt === null ? (
+                    <span className="text-zinc-400">Não fez check-in</span>
+                  ) : (
+                    dayjs().to(ateendee.checkedInAt)
+                  )}
+                </TableCell>
+                  
+                    <TableCell>
+                       
+                   <IconButton transparent={true}> 
+                    <MoreHorizontal className='size-4'/>
+                    </IconButton>
+                   </TableCell>
+               
+                 </TableRow>
+                    );
+                   }
+                     )
+                       }
+                         </tbody>
+                           <tfoot>
+                <tr>
+                       <td className='py-3 px-4 text-sm text-zinc-300' colSpan={3} >
+                       Mostrando {attendees.length} de {total} itens
+                       </td>
+                       <td className='py-3 px-4 text-sm text-zinc-300 text-right' colSpan={3} >
+               
+                       <div className='inline-flex items-center gap-8'>
+               
+                       <span>Página {page} de {totalPages}</span>     
+                       
+                       <div className='flex gap-1.5'>
+                           <IconButton onClick={goToFirstPage} disabled={page === 1}>
+                       <ChevronsLeft className="size-4" />
+                         </IconButton>
+                           <IconButton onClick={goToPreviousPage} disabled={page === 1}>
+                        <ChevronLeft className="size-4" />
+                        </IconButton>
+                      <IconButton onClick={goToNextPage} disabled={page === totalPages}>
+                       <ChevronRight className="size-4" />
+                        </IconButton>
+                         <IconButton onClick={goToLastPage} disabled={page === totalPages}>
+                         <ChevronsRight className="size-4" />
+                         </IconButton>
+                          </div>
+                          </div>
+                          </td>
+                       </tr>
+                     </tfoot>
+             </Table>
+    </div>
+ )
+}
