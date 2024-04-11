17b5f142230e8f791cc65c5935104e75e1814265
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-11 11:29:18 -0300

    Update attendee-list.tsx

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -55,7 +55,8 @@ if(search.length > 0){
     setTotal(data.total)
     console.log(data)
   }) 
-},[page,search])
+},
+[page,search])
 
 function onSearchInputChanged(event:ChangeEvent<HTMLInputElement>) {
   setSearch(event.target.value);
