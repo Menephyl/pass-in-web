5ff42a7e3d945cc7ba0a0b829610970f0f1e2970
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-11 10:55:50 -0300

    Update attendee-list.tsx

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -73,7 +73,8 @@ function goToPreviousPage() {
 
 function goToNextPage() {
   // setPage(page + 1);
-  const url  = new URLSearchParams(window.location.search) 
+
+  const url  = new URL(window.location.search) 
 
   searchParams.set('page',String(page + 1))
   
