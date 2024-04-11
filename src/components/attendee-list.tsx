906435466b05d12584447e5159eb645788c7c1cf
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-10 21:25:03 -0300

    Update attendee-list.tsx

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -150,10 +150,12 @@ function goToNextPage() {
                             disabled={page === 1}>
                         <ChevronLeft className="size-4" />
                         </IconButton>
-                      <IconButton onClick={goToNextPage} disabled={page === totalPages}>
+                      <IconButton onClick={goToNextPage}
+                       disabled={page === totalPages}>
                        <ChevronRight className="size-4" />
                         </IconButton>
-                         <IconButton onClick={goToLastPage} disabled={page === totalPages}>
+                         <IconButton onClick={goToLastPage} 
+                         disabled={page === totalPages}>
                          <ChevronsRight className="size-4" />
                          </IconButton>
                           </div>
