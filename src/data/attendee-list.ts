1b77e64a39368fce74739b99352d647e5befcf24
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-11 07:44:15 -0300

    Update attendee-list.ts

--- src/data/attendee-list.ts
+++ src/data/attendee-list.ts
@@ -3,7 +3,7 @@ export const attendees = Array.from({length:200}).map(()=> {
     return{
         id: faker.number.int({min:10000,max:20000 }),
         name: faker.person.fullName(),
-        email:faker.internet.email().toLowerCas,
+        email:faker.internet.email(),
         createdAt:faker.date.recent({days:30}),
         checkedInAt:faker.date.recent({days:7}),
     }
