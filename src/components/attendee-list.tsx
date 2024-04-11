dd05ff981b4dda8f492532d45ac1ab129318f184
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-11 10:00:19 -0300

    Update attendee-list.tsx

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -40,11 +40,11 @@ export function AttendeeList(){
   const totalPages = Math.ceil(total/10 )
 
 useEffect(() => {
-const url = new URL()
+const url = new URL(`http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees`)
 
 
 
-  fetch(`http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees?pageIndex=${page-1}`)
+  fetch(`?pageIndex=${page-1}`)
   .then(response => response.json())
   .then(data =>   {
     setAttendees(data.attendees)
