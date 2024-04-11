9d2431270ccb7ddc34ce9464fe786ea24ba58213
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-11 10:23:27 -0300

    Update attendee-list.tsx

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -43,7 +43,7 @@ useEffect(() => {
 const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
 
 url.searchParams.set('pageIndex',String(page -1))
-
+ 
 
   fetch(url)
   .then(response => response.json())
@@ -91,7 +91,7 @@ function goToNextPage() {
             onChange={onSearchInputChanged}
           />
         </div>
-        {search}
+    
       </div>
 
 
