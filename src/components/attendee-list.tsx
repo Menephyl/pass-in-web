f0e78c28abd3ccc2f74f59c44e468331c3b7e3af
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-11 11:28:06 -0300

    Update attendee-list.tsx

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -44,6 +44,9 @@ const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1
 
 url.searchParams.set('pageIndex',String(page -1))
  
+if(search.length > 0){
+  url.searchParams.set('query',search)
+}
 
   fetch(url)
   .then(response => response.json())
